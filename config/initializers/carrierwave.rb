fe56a1ac6ed456c0d4a9512df163b0d729e6c9ec
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-09-24 17:00:53 -0400

    Adding carrierwave, dropbox, and figaro for image uploads plus run 'figaro heroku:set -e production'

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = 'do5i64f8koq8qzg'
+  config.dropbox_app_secret = ENV['APP_SECRET']
+  config.dropbox_access_token = 'cytv3gc4jlz7nein'
+  config.dropbox_access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  config.dropbox_user_id = '61136989'
+  config.dropbox_access_type = 'app_folder'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923211241) do
+ActiveRecord::Schema.define(version: 20150924191805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150923211241) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
