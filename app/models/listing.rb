25c38258f72a7a3584ec1786207e5942438e4814
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-09-28 16:25:15 -0400

    Added support for Listing User Link

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,6 @@ class Listing < ActiveRecord::Base
 
    validates :name, :description, :price, :image, presence: true
    validates :price, numericality: { greater_than: 0 }
+
+   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+
+  has_many :listings, dependent: :destroy # listings to user that is deleted will also be deleted
 end
