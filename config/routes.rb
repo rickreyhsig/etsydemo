aadb7e6eea4a7cf2f9bf608293f7d7f1f967894e
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-10-01 18:20:19 -0400

    Add order history pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
 
     devise_for :users
     resources :listings do
-      resources :orders
+      resources :orders, only[:new, :create]
     end
     root 'listings#index'
     get 'pages/about'
     get 'pages/contact'
     get 'seller' => "listings#seller"
+    get 'sales' => "orders#sales"
+    get 'purchases' => "orders#purchases"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
