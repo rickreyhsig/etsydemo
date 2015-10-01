39cf99598331d2f062718f389601da1ca1942385
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-10-01 18:22:45 -0400

    Fix error on config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
     devise_for :users
     resources :listings do
-      resources :orders, only[:new, :create]
+      resources :orders, only: [:new, :create]
     end
     root 'listings#index'
     get 'pages/about'
